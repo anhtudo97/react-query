d22751773adae32507683b5f56c42021a190b410
anhtudo97 <anhtudo97@gmail.com>
2020-10-28 00:07:35 +0700

    feat: handle error

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,29 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 const HomePage = () => {
-  const queryInfor = useQuery("pokemon", () =>
-    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => res.json())
-  );
+  const queryInfor = useQuery("pokemon", async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    // if (true) {
+    //   throw new Error("Test error");
+    // }
+    return axios
+      .get("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json());
+  });
 
   console.log(queryInfor);
 
-  return <div>Welcome to Next.js!</div>;
+  return queryInfor.isLoading ? (
+    "loading ... "
+  ) : queryInfor.isError ? (
+    queryInfor.error.message
+  ) : (
+    <div>
+      {queryInfor.data?.map((result) => {
+        return <div key={result.name}>{result.name}</div>;
+      })}
+    </div>
+  );
 };
 
-export default HomePage; 
+export default HomePage;
