493da812956f058b06f0e0b56b61459c9b08db7d
anhtudo97 <anhtudo97@gmail.com>
2020-11-01 22:42:08 +0700

    feat: update config react query

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,30 @@
 import { useQuery } from "react-query";
+import { ReactQueryDevtools } from "react-query-devtools";
 import axios from "axios";
 
-const HomePage = () => {
-  const queryInfor = useQuery("pokemon", async () => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    // if (true) {
-    //   throw new Error("Test error");
-    // }
-    return axios
-      .get("https://pokeapi.co/api/v2/pokemon")
-      .then((res) => res.json());
-  });
+const Pokemon = () => {
+  const queryInfor = useQuery(
+    "pokemon",
+    async () => {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      // if (true) {
+      //   throw new Error("Test error");
+      // }
+      return axios
+        .get("https://pokeapi.co/api/v2/pokemon")
+        .then((res) => res.json());
+    },
+    {
+      // not updaing when you refocus on window
+      refetchOnWindowFocus: false,
+
+      // timeout reupdating date on window
+      staleTime: 5000,
+
+      // time to cache data on local use react-query
+      cacheTime: 5000
+    }
+  );
 
   console.log(queryInfor);
 
@@ -23,8 +37,17 @@ const HomePage = () => {
       {queryInfor.data?.map((result) => {
         return <div key={result.name}>{result.name}</div>;
       })}
+      <br />
+      {queryInfor.isFetching ? "Updating" : null}
     </div>
   );
 };
 
-export default HomePage;
+export default function App() {
+  <div>
+    <Pokemon />
+    <ReactQueryDevtools />
+  </div>;
+}
+
+export default App;
