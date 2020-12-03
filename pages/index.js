04c06a0791e80dc87bcf6b03d44f2f309215b62c
anhtudo97 <anhtudo97@gmail.com>
2020-12-03 22:06:40 +0700

    feat: custom hook react query

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
 import { useQuery } from "react-query";
 import { ReactQueryDevtools } from "react-query-devtools";
 import axios from "axios";
+import React from 'react'
 
-const Pokemon = () => {
-  const queryInfor = useQuery(
+const usePokemon = () => {
+  return useQuery(
     "pokemon",
     async () => {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      // if (true) {
-      //   throw new Error("Test error");
-      // }
       return axios
         .get("https://pokeapi.co/api/v2/pokemon")
-        .then((res) => res.json());
+        .then((res) => res.data.results);
     },
     {
       // not updaing when you refocus on window
@@ -22,11 +19,18 @@ const Pokemon = () => {
       staleTime: 5000,
 
       // time to cache data on local use react-query
-      cacheTime: 5000
+      cacheTime: 5000,
     }
   );
+};
 
-  console.log(queryInfor);
+const Count = () => {
+  const queryInfor = usePokemon();
+  return <h3>You have looking {queryInfor.data?.length} pokemon</h3>;
+};
+
+const Pokemon = () => {
+  const queryInfor = usePokemon();
 
   return queryInfor.isLoading ? (
     "loading ... "
@@ -43,11 +47,47 @@ const Pokemon = () => {
   );
 };
 
-export default function App() {
-  <div>
-    <Pokemon />
-    <ReactQueryDevtools />
-  </div>;
-}
+const PokemonSearch = ({ pokemon }) => {
+  const queryInfor = useQuery(
+    pokemon,
+    async () => {
+      return axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+        .then((res) => res.data);
+    },
+    {
+      // not updaing when you refocus on window
+      refetchOnWindowFocus: false,
 
-export default App;
+      // timeout reupdating date on window
+      staleTime: 5000,
+
+      // time to cache data on local use react-query
+      cacheTime: 5000,
+    }
+  );
+  return queryInfor.isLoading ? (
+    "loading ... "
+  ) : queryInfor.isError ? (
+    queryInfor.error.message
+  ) : (
+    <div>
+      {queryInfor.data?.sprites?.front_default ? (
+        <img src={queryInfor.data.sprites.front_default} alt="" />
+      ) : 'Pokemon not found'}
+    </div>
+  );
+};
+
+export default function App() {
+  const [pokemon, setPokemon] = React.useState("");
+  return (
+    <div>
+      <input value={pokemon} onChange={(e) => setPokemon(e.target.value)} />
+      <PokemonSearch pokemon={pokemon} />
+      {/* <Count />
+      <Pokemon /> */}
+      <ReactQueryDevtools />
+    </div>
+  );
+}
